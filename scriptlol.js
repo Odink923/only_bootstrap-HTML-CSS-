b9bd0a2ddfc9511326c25abec79bad3b0615afa6
Odink923 <odink923@gmail.com>
2024-01-02 17:37:56 +0200

    first commit

---
+++ scriptlol.js
@@ -0,0 +1,54 @@
+ document.getElementById('hider').onclick = function () {
+
+        if (document.getElementById('text').hidden == true) {
+            document.getElementById('text').hidden = false
+        } else if (document.getElementById('text').hidden == false) {
+            document.getElementById('text').hidden = true;
+        } else {
+            document.getElementById('text').hidden = false;
+        }
+    }
+ document.getElementById('hider1').onclick = function () {
+
+     if (document.getElementById('text1').hidden == true) {
+         document.getElementById('text1').hidden = false
+     } else if (document.getElementById('text1').hidden == false) {
+         document.getElementById('text1').hidden = true;
+     } else {
+         document.getElementById('text1').hidden = false;
+     }
+ }
+ document.getElementById('hider3').onclick = function () {
+
+     if (document.getElementById('text3').hidden == true) {
+         document.getElementById('text3').hidden = false
+     } else if (document.getElementById('text3').hidden == false) {
+         document.getElementById('text3').hidden = true;
+     } else {
+         document.getElementById('text3').hidden = false;
+     }
+ }
+ document.getElementById('hider4').onclick = function () {
+
+     if (document.getElementById('text4').hidden == true) {
+         document.getElementById('text4').hidden = false
+     } else if (document.getElementById('text4').hidden == false) {
+         document.getElementById('text4').hidden = true;
+     } else {
+         document.getElementById('text4').hidden = false;
+     }
+ }
+
+ document.getElementById('hider5').onclick = function () {
+
+     if (document.getElementById('text5').hidden == true) {
+         document.getElementById('text5').hidden = false
+     } else if (document.getElementById('text5').hidden == false) {
+         document.getElementById('text5').hidden = true;
+     } else {
+         document.getElementById('text5').hidden = false;
+     }
+ }
+
+
+
